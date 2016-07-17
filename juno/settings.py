79e0a6d5ea5b6a775ad4d84665da51a1e99b6fcc
Xiaodi Yu <xiaodi.yu@ou.edu>
2016-07-17 16:57:57 -0400

    No port number for postgres

--- juno/settings.py
+++ juno/settings.py
@@ -153,7 +153,7 @@ DATABASES = {
         # Set to empty string for localhost. Not used with sqlite3.
         "HOST": "localhost",
         # Set to empty string for default. Not used with sqlite3.
-        "PORT": "5342",
+        "PORT": "",
     }
 }
 
